d436a90fe8ac5c66b23cb404e4241a83d2186cb3
Arkadiusz Ostrowski <arek.ostrowski135@gmail.com>
2025-02-22 14:13:45 +0100

    Initial commit - base project setup

---
+++ src/main/java/com/simplenotes/simplenotes_backend/SimplenotesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.simplenotes.simplenotes_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplenotesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplenotesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplenotes/simplenotes_backend/SimplenotesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplenotes.simplenotes_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplenotesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
